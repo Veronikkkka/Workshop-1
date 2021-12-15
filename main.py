d32054f401f06116ae2ace66ec88ff9f6c019494
RodoDenDr0n <93664288+RodoDenDr0n@users.noreply.github.com>
2021-12-15 13:38:22 +0200

    Second push
    Реалізував функцію, що відповідає за стрільбу й почав робити ту, що відповідає за створення корабля

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-import random
+"""SHOOTING AND TRYING TO CREATE SHIP"""
 
 def create_table():
-    table = [[0 for i in range(10)] for j in range(10)]
+    """
+    This function creates table 10x10
+    :return:
+    """
+    table = [["0" for i in range(10)] for j in range(10)]
     return table
+table = create_table()
 
-def create_ships():
+
+def create_ship(table):
+    ship_info = []
+
+    print("Ось ваша поточна дошка: ")
+    for line in table:
+        print(line)
+
+    ship_facing = input("Поставте корабель: v - вертикально або h - горизонтально\n")
+    ship_info.append(ship_facing)
+
+    ship_length = input("Введіть довжину вашого корабля\n")
+    ship_info.append(ship_length)
+
+    ship_first_coordinate = list(map(int, input("Введіть через кому першу\
+     координату, куди ставити корабель\n").split(",")))
+    ship_info.append(ship_first_coordinate)
+
+    return ship_info
+print(create_ship(table))
+
+
+def place_ships():
     pass
 
-def shoot():
-    table = create_table()
+
+def shoot(table):
     shoot_coordinates = list(map(int, input("Введіть через кому координати, куди ви бажаєте здійснити постріл\n").split(",")))
 
     coordinate_x = shoot_coordinates[0] - 1
@@ -16,9 +43,14 @@ def shoot():
 
     if table[coordinate_x][coordinate_y] == 1:
         table[coordinate_x][coordinate_y] = "X"
+        print("Ви влучили! Зробіть постріл ще раз")
+        return shoot(table)
+    elif table[coordinate_x][coordinate_y] == "_":
+        print("По цьому полі вже здійснювалася стрільба! Спробуйте ще раз")
+        return shoot(table)
     else:
         table[coordinate_x][coordinate_y] = "_"
+        print("На жаль, ви не влучили")
+        return table
 
-    return table
-
-print(shoot())
+print(shoot(table))
